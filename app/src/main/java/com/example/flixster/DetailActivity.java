31714eeedcd52d6a576e13d50ca2c3f3d5c9f82a
Christine Lam <christinelam320@gmail.com>
2020-10-02 18:25:32 -0400

    Hide API Key

--- app/src/main/java/com/example/flixster/DetailActivity.java
+++ app/src/main/java/com/example/flixster/DetailActivity.java
@@ -24,7 +24,7 @@ import okhttp3.Headers;
 
 public class DetailActivity extends YouTubeBaseActivity {
 
-    private static final String YOUTUBE_API_KEY = "AIzaSyAOAfOTMe7RGORZfn5gBU3l8GHHww2Rgtg";
+    private static final String YOUTUBE_API_KEY = Keys.YOUTUBE_API_KEY;
     public static final String VIDEOS_URL = "https://api.themoviedb.org/3/movie/%d/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
 
     TextView tvTitle;
---
+++ app/src/main/java/com/example/flixster/Keys.java
@@ -0,0 +1,5 @@
+package com.example.flixster;
+
+public class Keys {
+    static final String YOUTUBE_API_KEY = "AIzaSyAOAfOTMe7RGORZfn5gBU3l8GHHww2Rgtg";
+}
